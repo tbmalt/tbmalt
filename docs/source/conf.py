b3404e6ea0e6ee2218be416cc64d6c80f3a04e17
Adam McSloy <mcsloy@uni-bremen.de>
2021-01-21 15:47:43 +0000

    Minor sphinx conf.py file fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,7 +58,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['source/_autosummary/*']
+exclude_patterns = ['source/_autosummary/*test*']
 
 autodoc_mock_imports = [
     'tbmalt.tests'
