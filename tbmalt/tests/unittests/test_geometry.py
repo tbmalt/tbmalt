925ed738cd05d97dfaea709d96d917d69a236bfd
FanGuozheng <gfan@uni-bremen.de>
2021-04-21 23:17:17 +0200

    add property and method for future DFTB 	modified:   ../../structures/geometry.py 	modified:   test_geometry.py

--- tbmalt/tests/unittests/test_geometry.py
+++ tbmalt/tests/unittests/test_geometry.py
@@ -27,7 +27,7 @@ def positions_data(device, batch=False, requires_grad=False):
 
 
 @fix_seed
-def atomic_numbers_data(device, batch=False):
+def numbers_data(device, batch=False):
     if not batch:
         return torch.tensor([6, 1, 1, 1, 1], device=device)
     else:
@@ -39,27 +39,27 @@ def atomic_numbers_data(device, batch=False):
 ##################
 # Geometry Basic #
 ##################
-def geometry_basic_helper(device, positions, atomic_numbers):
+def geometry_basic_helper(device, positions, numbers):
     """Function to reduce code duplication when testing basic functionality."""
     # Pack the reference data, if multiple systems provided
-    batch = isinstance(atomic_numbers, list)
+    batch = isinstance(numbers, list)
     if batch:
-        atomic_numbers_ref = pack(atomic_numbers)
+        numbers_ref = pack(numbers)
         positions_ref = pack(positions)
         positions_angstrom = [i / length_units['angstrom'] for i in positions]
     else:
-        atomic_numbers_ref = atomic_numbers
+        numbers_ref = numbers
         positions_ref = positions
         positions_angstrom = positions / length_units['angstrom']
 
     # Check 1: Ensure the geometry entity is correct constructed
-    geom_1 = Geometry(atomic_numbers, positions)
-    check_1 = (torch.allclose(geom_1.atomic_numbers, atomic_numbers_ref)
+    geom_1 = Geometry(numbers, positions)
+    check_1 = (torch.allclose(geom_1.numbers, numbers_ref)
                and torch.allclose(geom_1.positions, positions_ref))
     assert check_1, 'Geometry was not instantiated correctly'
 
     # Check 2: Check unit conversion proceeds as anticipated.
-    geom_2 = Geometry(atomic_numbers, positions_angstrom, units='angstrom')
+    geom_2 = Geometry(numbers, positions_angstrom, units='angstrom')
     check_2 = torch.allclose(geom_1.positions, geom_2.positions)
     assert check_2, 'Geometry failed to correctly convert length units'
 
@@ -70,7 +70,7 @@ def geometry_basic_helper(device, positions, atomic_numbers):
     # Test with a larger number of systems to ensure the string gets truncated.
     # This is only applicable to batched Geometry instances.
     if batch:
-        geom_3 = Geometry([atomic_numbers[0] for _ in range(10)],
+        geom_3 = Geometry([numbers[0] for _ in range(10)],
                           [positions[0] for _ in range(10)])
         _t2 = repr(geom_3)
         check_3 = '...' in _t2
@@ -84,7 +84,7 @@ def geometry_basic_helper(device, positions, atomic_numbers):
         new_device = {'cuda': torch.device('cpu'),
                       'cpu': torch.device('cuda:0')}[device.type]
         geom_1.to(new_device)
-        check_4 = (geom_1.atomic_numbers.device == new_device
+        check_4 = (geom_1.numbers.device == new_device
                    and geom_1.positions.device == new_device)
 
         assert check_4, '".to" method failed to set the correct device'
@@ -93,13 +93,13 @@ def geometry_basic_helper(device, positions, atomic_numbers):
 def test_geometry_single(device):
     """Test the basic single system functionality of the Geometry class"""
     geometry_basic_helper(device, positions_data(device),
-                          atomic_numbers_data(device))
+                          numbers_data(device))
 
 
 def test_geometry_batch(device):
     """Test the basic batch system functionality of the Geometry class"""
     geometry_basic_helper(device, positions_data(device, True),
-                          atomic_numbers_data(device, True))
+                          numbers_data(device, True))
 
 
 #####################
@@ -120,7 +120,7 @@ def test_geometry_single_from_ase_atoms(device):
 
     # Check 2: Check the tensors were placed on the correct device
     check_2 = (geom_1.positions.device == device
-               and geom_1.atomic_numbers.device == device)
+               and geom_1.numbers.device == device)
 
     assert check_2, 'from_ase_atoms did not place tensors on the correct device'
 
@@ -142,7 +142,7 @@ def test_geometry_batch_from_ase_atoms(device):
 
     # Check 2: Check the tensors were placed on the correct device
     check_2 = (geom_1.positions.device == device
-               and geom_1.atomic_numbers.device == device)
+               and geom_1.numbers.device == device)
 
     assert check_2, 'from_ase_atoms did not place tensors on the correct device'
 
@@ -150,19 +150,19 @@ def test_geometry_batch_from_ase_atoms(device):
 ####################
 # Geometry HDF5 IO #
 ####################
-def geometry_hdf5_helper(path, atomic_numbers, positions):
+def geometry_hdf5_helper(path, numbers, positions):
     """Function to reduce code duplication when testing the HDF5 functionality."""
     # Ensure any test hdf5 database is erased before running
     if os.path.exists(path):
         os.remove(path)
 
     # Pack the reference data, if multiple systems provided
-    batch = isinstance(atomic_numbers, list)
-    atomic_numbers_ref = pack(atomic_numbers) if batch else atomic_numbers
+    batch = isinstance(numbers, list)
+    numbers_ref = pack(numbers) if batch else numbers
     positions_ref = pack(positions) if batch else positions
 
     # Construct a geometry instance
-    geom_1 = Geometry(atomic_numbers, positions)
+    geom_1 = Geometry(numbers, positions)
 
     # Infer target device
     device = geom_1.positions.device
@@ -172,19 +172,19 @@ def geometry_hdf5_helper(path, atomic_numbers, positions):
         # Check 1: Write to the database and check that the written data
         # matches the reference data.
         geom_1.to_hdf5(db)
-        check_1 = (np.allclose(db['atomic_numbers'][()], atomic_numbers_ref.sft())
+        check_1 = (np.allclose(db['numbers'][()], numbers_ref.sft())
                    and np.allclose(db['positions'][()], positions_ref.sft()))
         assert check_1, 'Geometry not saved the database correctly'
 
         # Check 2: Ensure geometries are correctly constructed from hdf5 data
         geom_2 = Geometry.from_hdf5(db, device=device)
         check_2 = (torch.allclose(geom_2.positions, geom_1.positions)
-                   and torch.allclose(geom_2.atomic_numbers, geom_1.atomic_numbers))
+                   and torch.allclose(geom_2.numbers, geom_1.numbers))
         assert check_2, 'Geometry could not be loaded from hdf5 data'
 
         # Check 3: Make sure that the tensors were placed on the correct device
         check_3 = (geom_2.positions.device == device
-                   and geom_2.atomic_numbers.device == device)
+                   and geom_2.numbers.device == device)
         assert check_3, 'Tensors not placed on the correct device'
 
     # If this is a batch test then repeat test 2 but pass in a list of HDF5
@@ -192,7 +192,7 @@ def geometry_hdf5_helper(path, atomic_numbers, positions):
     if batch:
         os.remove(path)
         with h5py.File(path, 'w') as db:
-            for n, (an, pos) in enumerate(zip(atomic_numbers, positions)):
+            for n, (an, pos) in enumerate(zip(numbers, positions)):
                 Geometry(an, pos).to_hdf5(db.create_group(f'geom_{n + 1}'))
             geom_3 = Geometry.from_hdf5([db[f'geom_{i}'] for i in range(1, 4)])
             check_4 = torch.allclose(geom_3.positions.to(device), geom_1.positions)
@@ -206,14 +206,14 @@ def test_geometry_single_hdf5(device):
     """Ensure a Geometry instance can be witten to & read from an HDF5 database."""
     # Generate input data and run the tests
     geometry_hdf5_helper('.tbmalt_test_s.hdf5',
-                         atomic_numbers_data(device),
+                         numbers_data(device),
                          positions_data(device))
 
 
 def test_geometry_batch_hdf5(device):
     """Ensure Geometry instances can be witten to & read from an HDF5 database."""
     geometry_hdf5_helper('.tbmalt_test_b.hdf5',
-                         atomic_numbers_data(device, True),
+                         numbers_data(device, True),
                          positions_data(device, True))
 
 
@@ -226,7 +226,7 @@ def geometry_distance_helper(geom):
     device = geom.positions.device
     # Calculate the distance matrix and its reference
     dmat = geom.distances
-    if geom.atomic_numbers.dim() == 1:
+    if geom.numbers.dim() == 1:
         dmat_ref = distance_matrix(geom.positions.sft(), geom.positions.sft())
     else:
         dmat_ref = np.stack([distance_matrix(i, i) for i in geom.positions.sft()])
@@ -243,7 +243,7 @@ def geometry_distance_helper(geom):
 def test_geometry_distance_single(device):
     """Geometry single system distance test."""
     # Construct a geometry object
-    geom = Geometry(atomic_numbers_data(device),
+    geom = Geometry(numbers_data(device),
                     positions_data(device))
     geometry_distance_helper(geom)
 
@@ -251,6 +251,6 @@ def test_geometry_distance_single(device):
 def test_geometry_distance_batch(device):
     """Geometry batch system distance test."""
     # Construct a geometry object
-    geom = Geometry(atomic_numbers_data(device, True),
+    geom = Geometry(numbers_data(device, True),
                     positions_data(device, True))
     geometry_distance_helper(geom)
