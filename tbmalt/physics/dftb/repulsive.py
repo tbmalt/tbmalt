c33aead5ffbb8528c5b1d4af2ba283c08665fa3d
Jonas Müller <jonmue@uni-bremen.de>
2024-04-24 14:10:54 +0200

    cleaned up and added docstrings to the RepulsiveSplineFeed

--- tbmalt/physics/dftb/repulsive.py
+++ tbmalt/physics/dftb/repulsive.py
@@ -1,66 +1,72 @@
 from itertools import combinations_with_replacement
-from typing import List
+from typing import List, Optional, Dict, Tuple, Union
 import torch
 import numpy as np
 from ase.build import molecule
 from tbmalt import Geometry
 from tbmalt.io import skf
 
+Tensor = torch.Tensor
+
 torch.set_default_dtype(torch.float64)
 
-debugprint = 280
-
 class RepulsiveSplineFeed():
-    """Repulsive Feed using splines. Data is derived from a skf file.
+    r"""Repulsive Feed using splines for DFTB calculations. Data is derived from a skf file.
 
+    This feed uses splines to calculate the repulsive energy of a Geometry in the way it is defined for DFTB.
+
+    Arguments:
+        spline_data: Dictionary containing the the tuples of atomic number pairs as keys and the corresponding spline data as values.
     """
 
-    def __init__(self, spline_data):
+    def __init__(self, spline_data: Dict[Tuple, Tensor], device: Optional[torch.device] = None):
         self.spline_data = {frozenset(interaction_pairs):data for interaction_pairs,data in spline_data.items()}
 
-    def __call__(self, geo):
-        print('Called RepulsiveSplineFeed for geometry: \n', geo)
-        print('#'*debugprint)
-        print('#'*debugprint)
+    def __call__(self, geo: Union[Geometry, Tensor]) -> Tensor:
+        r"""Calculate the repulsive energy of a Geometry.
+
+        Arguments:
+            geo: Geometry object(s) for which the repulsive energy should be calculated. Either a single Geometry object or a batch of Geometry objects.
+
+        Returns:
+            Erep: The repulsive energy of the Geometry object(s).
+        """
         if geo.atomic_numbers.dim() == 1: #this means its not a batch
             batch_size = 1
         else:
             batch_size = geo.atomic_numbers.size(dim=0)
 
-        indxs = torch.tensor(range(geo.atomic_numbers.size(dim=-1)))
+        indxs = torch.tensor(range(geo.atomic_numbers.size(dim=-1))) #TODO add device   
         indx_pairs = torch.combinations(indxs)
         
         Erep = torch.zeros((batch_size))
-        print('Initial Erep: ', Erep)
-        print('-'*debugprint)
         for indx_pair in indx_pairs:
             atomnum1 = geo.atomic_numbers[..., indx_pair[0]].reshape((batch_size, ))
             atomnum2 = geo.atomic_numbers[..., indx_pair[1]].reshape((batch_size, ))
-            print('calculation for atomnum1: ', atomnum1, ' and atomnum2: ', atomnum2)
 
             distance = geo.distances[..., indx_pair[0], indx_pair[1]].reshape((batch_size, ))
-            print('Distance: ', distance)
-            print('-'*debugprint)
 
             for batch_indx in range(batch_size):
                 if atomnum1[batch_indx] == 0 or atomnum2[batch_indx] == 0:
                     continue
                 Erep[batch_indx] += self._repulsive_calc(distance[batch_indx], atomnum1[batch_indx], atomnum2[batch_indx])
-            print('Erep after pair: ', indx_pair, ' \n\tErep: ', Erep)
-            print('#'*debugprint)
 
         return Erep
 
-    def _repulsive_calc(self, distance, atomnum1, atomnum2):
-        print('Called _repulsive_calc for \n\tatomnum1: ', atomnum1, '\n\tatomnum2: ', atomnum2, '\n\tdistance: ', distance)
-        print('#'*debugprint)
+    def _repulsive_calc(self, distance: Tensor, atomnum1: Union[Tensor, int], atomnum2: Union[Tensor, int]) -> Tensor:
+        """Calculate the repulsive energy contribution between two atoms.
+
+        Arguments:
+            distance: The distance between the two atoms.
+            atomnum1: The atomic number of the first atom.
+            atomnum2: The atomic number of the second atom.
+
+        returns:
+            Erep: The repulsive energy contribution between the two atoms.
+        """
         spline = self.spline_data[frozenset((int(atomnum1), int(atomnum2)))]
-        #print('Spline data read: \n', spline)
         tail_start = spline.grid[-1]
-        print('Tail start: ', tail_start)
         exp_head_cutoff = spline.grid[0]
-        print('Exp head cutoff: ', exp_head_cutoff)
-        print('-'*debugprint)
 
         if distance < spline.cutoff:
             if distance > tail_start:
@@ -74,48 +80,85 @@ class RepulsiveSplineFeed():
         return 0
    
     @classmethod
-    def _exponential_head(cls, distance, coeffs):
-        print('Called _exponential_head for \n\tdistance: ', distance, '\n\tcoeffs: ', coeffs)
+    def _exponential_head(cls, distance: Tensor, coeffs: Tensor) -> Tensor:
+        r"""Exponential head calculation of the repulsive spline. 
+
+        Arguments:
+            distance: The distance between the two atoms.
+            coeffs: The coefficients of the exponential head.
+
+        Returns:
+            energy: The energy value of the exponential head.
+                The energy is calculated as :math:`\exp(-coeffs[0] \cdot r + coeffs[1]) + coeffs[2]`.
+        """
         a1 = coeffs[0].clone()
         a2 = coeffs[1].clone()
         a3 = coeffs[2].clone()
-        print('cloned coeffs values: \n')
-        print('a1: ', a1, ' a2: ', a2, ' a3: ', a3)
 
         return torch.exp(-a1*distance + a2) + a3
 
-#For some reason this spline method is less accurate than the ones below for spline and tail spline
-#    @classmethod 
-#    def _spline(cls, distance, start, coeffs):
-#        energy = coeffs[0].clone()
-#        rDiff = distance - start
-#        for coeff in coeffs[1:]:
-#            energy += coeff*rDiff
-#            rDiff *= rDiff
-#        return energy
-
     @classmethod 
-    def _spline(cls, distance, start, coeffs):
-        print('Called _spline for \n\tdistance: ', distance, ' \n\tstart: ', start, '\n\tcoeffs: ', coeffs)
+    def _spline(cls, distance: Tensor, start: Tensor, coeffs: Tensor) -> Tensor:
+        r"""3rd order polynomial Spline calculation of the repulsive spline.
+
+        Arguments:
+            distance: The distance between the two atoms.
+            start: The start of the spline segment.
+            coeffs: The coefficients of the polynomial.
+
+        Returns:
+            energy: The energy value of the spline segment.
+                The energy is calculated as :math:`coeffs[0] + coeffs[1]*(distance - start) + coeffs[2]*(distance - start)^2 + coeffs[3]*(distance - start)^3`.
+        """
         rDiff = distance - start
-        print('   rDiff: ', rDiff)
         energy = coeffs[0] + coeffs[1]*rDiff + coeffs[2]*rDiff**2 + coeffs[3]*rDiff**3
-        print('   Energy: ', energy)
         return energy
 
     @classmethod 
-    def _tail(cls, distance, start, coeffs):
-        print('Called _tail for \n\tdistance: ', distance, '\n\tstart: ', start, '\n\tcoeffs: ', coeffs)
+    def _tail(cls, distance: Tensor, start: Tensor, coeffs: Tensor) -> Tensor:
+        r"""5th order polynomial trailing tail calculation of the repulsive spline.
+
+        Arguments:
+            distance: The distance between the two atoms.
+            start: The start of the trailing tail segment.
+            coeffs: The coefficients of the polynomial.
+        
+        Returns:
+            energy: The energy value of the tail.
+                The energy is calculated as :math:`coeffs[0] + coeffs[1]*(distance - start) + coeffs[2]*(distance - start)^2 + coeffs[3]*(distance - start)^3 + coeffs[4]*(distance - start)^4 + coeffs[5]*(distance - start)^5`.
+        """
         rDiff = distance - start
-        print('   rDiff: ', rDiff)
         energy = coeffs[0] + coeffs[1]*rDiff + coeffs[2]*rDiff**2 + coeffs[3]*rDiff**3 + coeffs[4]*rDiff**4 + coeffs[5]*rDiff**5
-        print('   Energy: ', energy)
         return energy
 
 
 
     @classmethod
-    def from_database(cls, path: str, species: List[int], **kwargs) -> 'RepulsiveSplineFeed':
+    def from_database(cls, path: str, species: List[int], device: Optional[torch.device] = None) -> 'RepulsiveSplineFeed':
+        r"""Instantiate instance from a HDF5 database of Slater-Koster files.
+
+        Instantiate a `RepulsiveSplineFeed` instance from a HDF5 database for the specified elements.
+
+        Arguments:
+            path: Path to the HDF5 file from which the repulsive interaction data should be taken.
+            species: List of atomic numbers for which the repulsive spline data should be read.
+            device: Device on which the feed object and its contents resides.
+
+        Returns:
+            repulsive_feed: A `RepulsiveSplineFeed` instance.
+
+        Notes:
+            This method will not instantiate `SkFeed` instances directly
+            from human readable skf files, or a directory thereof. Thus, any
+            such files must first be converted into their binary equivalent.
+            This reduces overhead & file format error instabilities. The code
+            block provide below shows how this can be done:
+
+            >>> from tbmalt.io.skf import Skf
+            >>> Zs = ['H', 'C', 'Au', 'S']
+            >>> for file in [f'{i}-{j}.skf' for i in Zs for j in Zs]:
+            >>>     Skf.read(file).write('my_skf.hdf5')
+        """
         interaction_pairs = combinations_with_replacement(species, r=2)
         return cls({interaction_pair: skf.Skf.read(path, interaction_pair).r_spline for interaction_pair in interaction_pairs})
 
--- tests/unittests/test_dftb_repulsive.py
+++ tests/unittests/test_dftb_repulsive.py
@@ -56,14 +56,9 @@ def molecules(device) -> List[Geometry]:
                          units='angstrom')
 
     return [H2, CH4, C2H2Au2S3]
-    #return [H2]
-    #return [CH4]
-    #return [C2H2Au2S3]
 
 references = [0.0058374104, 0.0130941359, 47.8705446288] #H2, CH4, C2H2Au2S3 in Hartree
-#references = [0.0058374104]
-#references = [0.0130941359] #H2, CH4, C2H2Au2S3 in Hartree
-#references = [47.8705446288] #H2, CH4, C2H2Au2S3 in Hartree
+
 # Single
 def test_repulsivefeed_single(skf_file: str, device):
 
@@ -73,35 +68,27 @@ def test_repulsivefeed_single(skf_file: str, device):
     for mol, repulsive_ref in zip(molecules(device), references):
         repulsive_energy = repulsive_feed(mol)
         
-#        check_1 = torch.allclose(repulsive_energy, torch.tensor([repulsive_ref]), atol=0, rtol=1E-5) #Works for H2
-#        check_1 = torch.allclose(repulsive_energy, torch.tensor([repulsive_ref]), atol=1E-20) #works for H2
-#        check_1 = torch.allclose(repulsive_energy, torch.tensor([repulsive_ref]), atol=0, rtol=1E-3) #Works for H2 and CH4 and C2H2Au2S3
-#        check_1 = torch.allclose(repulsive_energy, torch.tensor([repulsive_ref]), atol=1E-5) #works for H2 and CH4 
-#        check_1 = torch.allclose(repulsive_energy, torch.tensor([repulsive_ref]), atol=0, rtol=1E-4) #Works for H2 and C2H2Au2S3
-#        check_1 = torch.allclose(repulsive_energy, torch.tensor([repulsive_ref]), atol=1E-2) #works for H2 and CH4 and C2H2Au2S3
-        #check_1 = torch.allclose(repulsive_energy, torch.tensor([repulsive_ref]), atol=1E-5, rtol=1E-4) 
-#        check_1 = torch.allclose(repulsive_energy, torch.tensor([repulsive_ref]), atol=1E-6)
         check_1 = torch.allclose(repulsive_energy, torch.tensor([repulsive_ref]), rtol=0, atol=1E-10)
-        check_2 = repulsive_energy.device == device
+        check_2 = repulsive_energy.device == device #TODO add device to repulsive_feed
 
         assert check_1, f'RepulsiveSplineFeed repulsive energy outside of tolerance (Geometry: {mol}, Energy: {repulsive_energy}, Reference: {repulsive_ref})'
         assert check_2
 
 # Batch
-#def test_repulsivefeed_batch(skf_file: str, device):
-#    repulsive_feed = RepulsiveSplineFeed.from_database(skf_file, species=[1, 6, 16, 79])
-#    mols = reduce(lambda i, j: i+j, molecules(device))
-#
-#    repulsive_energy = repulsive_feed(mols)
-#    
-#    repulsive_ref_single = torch.tensor([])
-#    for mol in molecules(device):
-#        repulsive_ref_single = torch.cat((repulsive_ref_single, repulsive_feed(mol)))
-#
-#    check_1 = torch.allclose(repulsive_energy, repulsive_ref_single, atol=1e-9, rtol=0)
-#    check_2 = repulsive_energy.device == device
-#
-#    assert check_1, f'RepulsiveSplineFeed batch energy difference to single calculation outside of tolerance (Batch: {repulsive_energy}, Single: {repulsive_ref_single}) '
-#
-#
-#
+def test_repulsivefeed_batch(skf_file: str, device):
+    repulsive_feed = RepulsiveSplineFeed.from_database(skf_file, species=[1, 6, 16, 79])
+    mols = reduce(lambda i, j: i+j, molecules(device))
+
+    repulsive_energy = repulsive_feed(mols)
+    
+    repulsive_ref_single = torch.tensor([])
+    for mol in molecules(device):
+        repulsive_ref_single = torch.cat((repulsive_ref_single, repulsive_feed(mol)))
+
+    check_1 = torch.allclose(repulsive_energy, repulsive_ref_single, atol=1e-9, rtol=0)
+    check_2 = repulsive_energy.device == device #TODO add device to repulsive_feed
+
+    assert check_1, f'RepulsiveSplineFeed batch energy difference to single calculation outside of tolerance (Batch: {repulsive_energy}, Single: {repulsive_ref_single}) '
+
+
+
