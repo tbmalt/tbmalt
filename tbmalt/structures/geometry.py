Added example for force calculation using autograd, doesnt currently work for batches, need to verify if calculated forces are correct. Also added .clone() to the distance calculation of the geometry, since this fixes error: inplace operation

tbmalt/structures/geometry.py
@@ -271,7 +271,7 @@ class Geometry:
         """Distance matrix between atoms in the system."""
         # Todo: Modify to account for PBC
 
-        dist = torch.cdist(self.positions, self.positions, p=2)
+        dist = torch.cdist(self.positions, self.positions, p=2).clone()
         # Ensure padding area is zeroed out
         dist[self._mask_dist] = 0
 