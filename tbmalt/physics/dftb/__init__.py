fixes error regarding non padding in finite diff calculation for overlap and h0 matrix, caused by the shift of padded geometry

tbmalt/physics/dftb/__init__.py
@@ -460,10 +460,14 @@ class Dftb1(Calculator):
         #TODO something in this summation seems to be wrong (it returns non padded tensor for first batch)
         force = torch.einsum('...mn,...acmn->...ac', density, dh0) + torch.einsum('...mn,...acmn->...ac', rho_weighted, doverlap)
 
+        #force = (density * dh0).sum(-1).sum(-1)
+
+        #force = (density.unsqueeze(1).unsqueeze(1)*dh0).sum(-1).sum(-1)
+
+
         print(self.geometry.atomic_numbers)
-        #print(dh0[0])
-        #print(doverlap[0])
-        print(rho_weighted[0])
+        #print(dh0[0].size())
+        #print(self.hamiltonian[0].size())
 
         return force
 
@@ -502,6 +506,11 @@ class Dftb1(Calculator):
 
             doverlap[..., int(atom_idx / 3), atom_idx % 3, :, :] = (temp_overlap - self.overlap) / delta
             dh0[..., int(atom_idx / 3), atom_idx % 3, :, :] = (temp_h0 - self.hamiltonian) / delta
+
+        # Mask to add padded values which have been falsely calculated before
+        mask = self.geometry.atomic_numbers == 0
+        doverlap[mask] = torch.zeros_like(doverlap[mask])
+        dh0[mask] = torch.zeros_like(dh0[mask])
 
         return doverlap, dh0
 