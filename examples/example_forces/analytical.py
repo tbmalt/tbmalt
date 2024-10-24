made the derivative calculation for the repulsive return the correct values, but it doesnt work for batched systems as of now

examples/example_forces/analytical.py
@@ -8,30 +8,42 @@ from tbmalt.physics.dftb.feeds import SkFeed, SkfOccupationFeed, HubbardFeed, Re
 # Define global constants
 torch.set_default_dtype(torch.float64)
 torch.autograd.set_detect_anomaly(True)
-#torch.set_printoptions(precision=15, sci_mode=False, linewidth=200, profile="full")
+torch.set_printoptions(precision=15, sci_mode=False, linewidth=200, profile="full")
+
+print('-------------------------')
+print('Analytical')
 
 # File with the sk data
 path = './auorg.hdf5'
 
-species = [1, 6, 8]
+#species = [1, 6, 8]
+species = [1]
 
-shell_dict = {1: [0], 6: [0,1], 8: [0, 1]}
+#shell_dict = {1: [0], 6: [0,1], 8: [0, 1]}
+shell_dict = {1: [0]}
 # Set up geometry
-H2O = Geometry(torch.tensor([8, 1, 1]), 
-               torch.tensor([[0.0, 0.0, 0.0],
-                             [0.0, 0.8, -0.5],
-                             [0.0, -0.8, 0.5]], requires_grad=False)
-               )
-CO2 = Geometry(torch.Tensor([6, 8, 8]), 
-               torch.tensor([[0.0, 0.0, 0.0],
-                             [0.0, 0.0, 1.16],
-                             [0.0, 0.0, -1.16]], requires_grad=False)
-               )
+#H2O = Geometry(torch.tensor([8, 1, 1]), 
+#               torch.tensor([[0.0, 0.0, 0.0],
+#                             [0.0, 0.8, -0.5],
+#                             [0.0, -0.8, -0.5]], requires_grad=False),
+#               units='angstrom'
+#               )
+#CO2 = Geometry(torch.Tensor([6, 8, 8]), 
+#               torch.tensor([[0.0, 0.0, 0.0],
+#                             [0.0, 0.0, 1.16],
+#                             [0.0, 0.0, -1.16]], requires_grad=False)
+#               )
 
-geos = Geometry.from_ase_atoms([molecule('H2O'), molecule('CH4')])
+#geos = Geometry.from_ase_atoms([molecule('H2O'), molecule('CH4')])
 #print(geos._positions)
 #print(geos.atomic_numbers)
 
+H2 = Geometry(torch.tensor([1, 1]), 
+               torch.tensor([[0.0, 0.0, 0.0],
+                             [0.0, 0.0, 0.5]], requires_grad=False),
+               units='angstrom'
+               )
+geos = H2
 
 orbital_info = OrbitalInfo(geos.atomic_numbers, shell_dict, shell_resolved=False)
 
@@ -47,9 +59,18 @@ repulsive_feed = RepulsiveSplineFeed.from_database(path, species)
 dftb_calculator = Dftb1(hamiltonian_feed, overlap_feed, occupation_feed, r_feed=repulsive_feed)
 
 # Run a SCF calculation
-dftb_calculator(geos, orbital_info)
+energy = dftb_calculator(geos, orbital_info)
+print('Energy:', energy)
 
-dftb_calculator.total_energy
+# Get total energy
+total_energy = dftb_calculator.total_energy
+print('Total energy:', total_energy)
 
+#Get repulsive energy
+repulsive_energy = dftb_calculator.repulsive_energy
+print('Repulsive energy:', repulsive_energy)
+
+#Get forces
 forces = dftb_calculator.forces
-print(forces)
+print('Forces:', forces)
+

examples/example_forces/autograd.py
@@ -1,29 +1,41 @@
 import torch
 from tbmalt import Geometry, OrbitalInfo
 from tbmalt.ml.module import Calculator
-from tbmalt.physics.dftb import Dftb2
+from tbmalt.physics.dftb import Dftb1
 from tbmalt.physics.dftb.feeds import SkFeed, SkfOccupationFeed, HubbardFeed, RepulsiveSplineFeed
 
 # Define global constants
 torch.set_default_dtype(torch.float64)
 torch.autograd.set_detect_anomaly(True)
 
+print('-------------------------')
+print('autograd')
+
 # File with the sk data
 path = './auorg.hdf5'
 
-species = [1, 8]
+#species = [1, 8]
+species = [1]
 
-shell_dict = {1: [0], 8: [0, 1]}
+#shell_dict = {1: [0], 8: [0, 1]}
+shell_dict = {1: [0]}
 
 # Set up geometry
-H2O = Geometry(torch.tensor([8, 1, 1]), 
+#H2O = Geometry(torch.tensor([8, 1, 1]), 
+#               torch.tensor([[0.0, 0.0, 0.0],
+#                             [0.0, 0.8, -0.5],
+#                             [0.0, -0.8, -0.5]], requires_grad=True),
+#               units='angstrom'
+#               )
+#H2O.positions.requires_grad_(True)
+#print('Positions:', H2O.positions)
+
+H2 = Geometry(torch.tensor([1, 1]), 
                torch.tensor([[0.0, 0.0, 0.0],
-                             [0.0, 0.8, -0.5],
-                             [0.0, -0.8, -0.5]], requires_grad=True),
+                             [0.0, 0.0, 0.5]], requires_grad=True),
                units='angstrom'
                )
-#H2O.positions.requires_grad_(True)
-print('Positions:', H2O.positions)
+H2O = H2
 
 orbital_info = OrbitalInfo(H2O.atomic_numbers, shell_dict, shell_resolved=False)
 
@@ -35,7 +47,8 @@ hubbard_feed = HubbardFeed.from_database(path, species)
 repulsive_feed = RepulsiveSplineFeed.from_database(path, species)
 
 # Set up the calculator
-dftb_calculator = Dftb2(hamiltonian_feed, overlap_feed, occupation_feed, hubbard_feed, r_feed=repulsive_feed)
+#dftb_calculator = Dftb2(hamiltonian_feed, overlap_feed, occupation_feed, hubbard_feed, r_feed=repulsive_feed)
+dftb_calculator = Dftb1(hamiltonian_feed, overlap_feed, occupation_feed, r_feed=repulsive_feed)
 
 # Run a SCF calculation
 energy = dftb_calculator(H2O, orbital_info)

tbmalt/physics/dftb/__init__.py
@@ -448,6 +448,7 @@ class Dftb1(Calculator):
         doverlap, dh0 = self._finite_diff_overlap_h0()
         # Use the already calculated density matrix rho_mu,nu
         density = self.rho
+        print('Density:', density)
         # Calculate energy weighted density matrix
         temp_dens = torch.einsum(  # Scaled occupancy values
             '...i,...ji->...ji', torch.sqrt(self.occupancy), self.eig_vectors)
@@ -456,13 +457,15 @@ class Dftb1(Calculator):
             '...i,...ji->...ji', self.eig_values * torch.sqrt(self.occupancy), self.eig_vectors)
         
         rho_weighted = temp_dens_weighted @ temp_dens.transpose(-1, -2).conj()
+        print('Rho weighted:', rho_weighted)
 
         #TODO something in this summation seems to be wrong (it returns non padded tensor for first batch)
-        force = - torch.einsum('...nm,...acmn->...ac', density, dh0) + torch.einsum('...nm,...acmn->...ac', rho_weighted, doverlap) - self.r_feed.dErep
+        force = - torch.einsum('...nm,...acmn->...ac', density, dh0) + torch.einsum('...nm,...acmn->...ac', rho_weighted, doverlap) #- self.r_feed.dErep
+        #force = self.r_feed.dErep
 
         return force
 
-    def _finite_diff_overlap_h0(self, delta=0.025):
+    def _finite_diff_overlap_h0(self, delta=1.0e-2):
         """Calculates the gradient of the overlap using finite differences
         
         Arguments: